9ead2b68497280e28c47c1ab95d6873c015eb417
yyang200 <48287074+Secretlovelmy@users.noreply.github.com>
2021-04-02 12:21:20 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 杨宇 on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
